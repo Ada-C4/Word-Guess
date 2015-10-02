5f8699246dcdbfee8bf0f61634a4dff3c7c21a8a
Daphne <dg1181@nyu.edu>
2015-10-02 09:31:53 -0700

    Added class and method structure

---
+++ word-guess.rb
@@ -0,0 +1,17 @@
+# This will be our Mouse Party Game. By Jenna and Daphne.
+
+# In this class we will contain visual aspects, such as ASCII art
+class Art
+end
+
+# In this class we will contain the visual aspects of the word
+class Word
+end
+
+# In this class we will control the workings of the game
+class Game
+end
+
+# Where the game is run from
+def play_mouse_party
+end
