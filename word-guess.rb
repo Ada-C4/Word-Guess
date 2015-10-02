b02caaa44ea489f3f3765ae14bd2f7aa053ed2d1
Desiree <desireepoland@gmail.com>
2015-10-02 16:00:16 -0700

    Add imagery to gameplay

--- word-guess.rb
+++ word-guess.rb
@@ -12,6 +12,7 @@ class Game
     @wrong_guesses = []
     @correct_guesses = []
     @turn_array = []
+    @image = print_image
   end
 
   def generate_answer
@@ -25,7 +26,9 @@ class Game
   end
 
   def print_board(guess)
-    puts "Word = "
+    2.times {puts}
+    print_image
+    print "\n\n\nWord: "
     @answer_array.each do |letter|
       if letter == guess || @correct_guesses.include?(letter)
         print letter
@@ -34,7 +37,7 @@ class Game
         print "_"
       end
     end
-    puts "\nWrong guesses:\n#{@wrong_guesses}"
+    puts "\n\Wrong guesses:\n#{@wrong_guesses.join(" ")}"
 
    @turn_array = @guess_array
    @guess_array = []
@@ -48,9 +51,25 @@ class Game
     end
   end
 
-  def image
-    if wrong_guesses.length == 0
-      puts pacman
+  def print_image
+    if @wrong_guesses.length == 0
+      puts PACMAN8
+    elsif @wrong_guesses.length == 1
+      puts PACMAN7
+    elsif @wrong_guesses.length == 2
+      puts PACMAN6
+    elsif @wrong_guesses.length == 3
+      puts PACMAN5
+    elsif @wrong_guesses.length == 4
+      puts PACMAN4
+    elsif @wrong_guesses.length == 5
+      puts PACMAN3
+    elsif @wrong_guesses.length == 6
+      puts PACMAN2
+    elsif @wrong_guesses.length == 7
+      puts PACMAN1
+    else
+      puts PACMAN
     end
   end
 
@@ -58,18 +77,15 @@ class Game
   end
 
   def play_game
-    puts PACMAN8
+    start_game
     until game_lost? || game_won?
       guess = gets.chomp.upcase
-
       check_guess(guess)
       print_board(guess)
-
-      puts "\nYour guess was #{guess}."
-
+      puts "\nPlease enter another letter:"
     end
     if game_lost?
-      puts "GAME OVER: You lost :("
+      puts "GAME OVER"
     else
       puts "GAME OVER: YOU WON! :)"
     end
@@ -83,38 +99,13 @@ class Game
     @turn_array == @answer_array
   end
 
+  def start_game
+    puts "Welcome to Word Guess!"
+    puts "Enter letters to guess the secret word before pacman eats everything!"
+    puts "What is your guess?"
+  end
 
 end
 
-
-pacman =
-<<-PACMAN
-    OZZOZZZZO$ZZZO
-,OZZZZZZZZZZZZZ$Z$ZOI
-ZOOZZZZZZZZZZZZZZZZZ$ZO
-8ZZZ$ZZZZZZZZZZZZZZZZO$Z$
-ZZZZZZZZZZZZZZZZZZZZZZ$ZI
-O$ZZZZZZZZZZZZZZZZZZZZZ=
-ZZZZZZZZZZZZZZZZZ$$Z$O
-ZZZZZZZZZZZZZZZZZZOZ
-ZZZZZZZZZZZZZZZZOI     NMMM       MNMM       MMNM      MMMM      MMMM      MMMM       MMM       NNM
-ZZZZZZZZZZZZZZZ?      MMNMMM     MMNMNN     MMMMMM    NMMMMM    MMMMMM    MMMMMM     MMNMM     NMMMM
-ZZZZZZZZZZZZZZZOOI     MMMM       MMNM       MNMN      MNNM      MNMM      NMMM       MMM       MNM
-ZZZZZZZZZZZZZZZZZ$Z:
-ZZZZZZZZZZZZZZZZZZZZZO
-ZZZZZZZZZZZZZZZZZZZZZ$Z
-ZZZZZZZZZZZZZZZZZZZZZZZZ
-OZZZZZZZZZZZZZZZZZZZZZ$Z8
-Z$Z$ZZZZZZZZZZZZZZZZO$Z
-O$ZZZZZZZZZZZZZZZZ$7
-+Z$ZZZZZZZ$ZZZZ
-    ?7ZZZZZ$
-
-PACMAN
-
 a = Game.new
-puts "Welcome to Word Guess!"
-puts "Enter letters to guess the secret word before pacman eats everything!"
-puts "What is your guess?"
-
 a.play_game
