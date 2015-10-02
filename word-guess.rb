bd6dc4c04dc5fa2e208656dd277c8d1bc530f283
Desiree <desireepoland@gmail.com>
2015-10-02 11:21:57 -0700

    Add methods to check guess

--- word-guess.rb
+++ word-guess.rb
@@ -10,27 +10,57 @@ WORDS = %w(flower pancake caterpillar avocado dragon sushi)
 class Game
   def initialize
     @answer = generate_answer
+    @answer_array = generate_answer_array
+    @wrong_guesses = []
+    @correct_guesses = []
   end
 
   def generate_answer
-    answer = WORDS[rand(0..(WORDS.length - 1))]
+    answer = WORDS[rand(0..(WORDS.length - 1))].upcase
     return answer
   end
 
-  def new_guess(guess)
-
+  def generate_answer_array
+    answer_array = @answer.split("")
+    return answer_array
   end
 
+  def print_board(guess)
+    @answer_array.each do |letter|
+      if guess == letter
+        print letter
+      else
+        print "_"
+      end
+    end
+  end
+
+  def image
+  end
+
+  def new_guess(guess)
+  end
+
+  def check(guess)
+    if @answer_array.include?(guess)
+      puts "yay"
+      puts @answer
+    else
+      puts "boo"
+      @wrong_guesses.push(guess)
+      puts @answer
+    end
+  end
+
+
 end
 
-class Board
-
-end
-
+a = Game.new
 puts "Welcome to Word Guess!"
 puts "Enter letters to guess the secret word before pacman eats everything!"
 puts "What is your guess?"
 
-guess = gets.chomp
+guess = gets.chomp.upcase
+a.print_board(guess)
 
 puts "Your guess was #{guess}."
