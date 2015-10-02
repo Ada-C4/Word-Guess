d59bf624c6c95b46cc7cc806d5b93ae838035eb1
Jenna Nichols <jennaplusplus@gmail.com>
2015-10-02 09:51:43 -0700

    added word list and modified basic game file

--- random-word-test.rb
+++ random-word-test.rb
@@ -1,22 +1,3 @@
-# This will be our Mouse Party Game. By Jenna and Daphne.
-# updated with no content
-
-# In this class we will contain visual aspects, such as ASCII art
-class Art
-end
-
-# In this class we will contain the visual aspects of the word
-class Word
-end
-
-# In this class we will control the workings of the game
-class Game
-end
-
-# Where the game is run from
-def play_mouse_party
-end
-
 # word list is from http://www.desiquintans.com/nounlist
 # "wordlist.txt" is saved in the same directory as this ruby file
 words = open("wordlist.txt")
