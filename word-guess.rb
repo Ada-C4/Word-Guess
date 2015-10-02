09d30797a02e1ff95de3c3e6a8dead8dc0ed4b6e
Daphne <dg1181@nyu.edu>
2015-10-02 10:26:14 -0700

    More changes

--- word-guess.rb
+++ word-guess.rb
@@ -6,37 +6,60 @@
 MAX_GUESSES = 1
 
 class Art
+  mouse = """
+  .--,       .--,
+  ( (  \\.---./ ) )
+  '.__/o   o\\__.'
+     {=  ^  =}
+      >  -  <
+     /       \\
+    //       \\\\
+   //|   .   |\\\\
+   \"'\\       /'\"_.-~^`'-.
+      \\  _  /--'         `
+    ___)( )(___
+   (((__) (__)))
+   """
+  puts mouse
+
+def cheese_print(number_guesses)
+  cheese_1 = "   ____  "
+  cheese_2 = "  /|o  | "
+  cheese_3 = " /o|  o| "
+  cheese_4 = "/o_|_o_| "
+
+
+  puts cheese_1 * number_guesses
+  puts cheese_2 * number_guesses
+  puts cheese_3 * number_guesses
+  puts cheese_4 * number_guesses
 end
 
-# In this class we will contain the visual aspects of the word
-class Word
-  def word_gen
-    # word list is from http://www.desiquintans.com/nounlist
-    # "wordlist.txt" is saved in the same directory as this ruby file
-    words = open("wordlist.txt")
+def underscores
+  underscore = "_ " * rando.length
 
-
-    array_of_words = words.readlines
-
-    array_of_words.map! do |word|
-      word.strip
-    end
-
-    rando = array_of_words[rand(0...array_of_words.length)]
-
-    while rando.length < 5 || rando.length > 7
-      rando = array_of_words[rand(0...array_of_words.length)]
-    end
-
-
-    puts rando
-    words.close
-  end
+end
 
 end
 
 # In this class we will control the workings of the game
 class Game
+
+  def word_gen
+    # word list is from http://www.desiquintans.com/nounlist
+    # "wordlist.txt" is saved in the same directory as this ruby file
+    words = open("wordlist.txt")
+    array_of_words = words.readlines
+    array_of_words.map! do |word|
+      word.strip
+    end
+    rando = array_of_words[rand(0...array_of_words.length)]
+    while rando.length < 5 || rando.length > 7
+      rando = array_of_words[rand(0...array_of_words.length)]
+    end
+    words.close
+    return rando
+  end
 
 end
 
@@ -44,37 +67,9 @@ end
 def play_mouse_party
   print "Give me a letter to guess? "
   guess = gets.chomp
-
   puts guess
 
+  mouse_game = Game.new
 end
 
 play_mouse_party
-
-
-mouse = """
-.--,       .--,
-( (  \\.---./ ) )
-'.__/o   o\\__.'
-   {=  ^  =}
-    >  -  <
-   /       \\
-  //       \\\\
- //|   .   |\\\\
- \"'\\       /'\"_.-~^`'-.
-    \\  _  /--'         `
-  ___)( )(___
- (((__) (__)))
- """
-puts mouse
-
-cheese_1 = "   ____  "
-cheese_2 = "  /|o  | "
-cheese_3 = " /o|  o| "
-cheese_4 = "/o_|_o_| "
-
-
-puts cheese_1 * 5
-puts cheese_2 * 5
-puts cheese_3 * 5
-puts cheese_4 * 5
