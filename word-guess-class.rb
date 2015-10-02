d7e5942930c09bc5fe90bb648ac659f6644a8e70
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-02 15:10:14 -0700

    Another somewhat updated version

--- word-guess-class.rb
+++ word-guess-class.rb
@@ -1,15 +1,16 @@
 # Setting global variables
 
 class Game
-attr_reader :answer, :guesses, :scores, :guess_index, :slots,
+attr_reader :answer, :guesses, :scores, :guess_index, :slots
 
   def initialize
     @answer = [] # Will pull a new word each game
     @guesses = [] # No guesses yet in the array
-    @scores = :unknown # Will be determined at the end of the game
-    @display = Board.new
+    #@scores = :unknown # Will be determined at the end of the game
+    #@display = Board.new
     @slots = []
     @guess_index = 0
+    @max_wrong = 0
   end
 
   def play
@@ -17,7 +18,7 @@ attr_reader :answer, :guesses, :scores, :guess_index, :slots,
     generate_answer
     generate_slots
     game_play
-    game_end
+    #game_end
   end
 
 
@@ -34,28 +35,59 @@ attr_reader :answer, :guesses, :scores, :guess_index, :slots,
   def generate_slots
     @answer.each do |x|
       x = "_"
-    @slots.push(x)
+      @slots.push(x)
     end
   end
 
   def game_play
-    # Get user input
-    # Compare input to @answer
-    # if true, replace the slot in the slots array with the letter
-    # if false, change ASCII art
-    # loop until you read max# wrong answers OR @slots = @answers
-  end
-
-  def letter_match(guess)
-    @answer.each do |l|
-      if guess == l
-        @guess_index = @answer.find_index(l)
-        @slots[@guess_index] = @answer[@guess_index]
-      else
-        puts "wrong answer"
-      end
+    # loop until game end
+    until @max_wrong == 10 || @slots == @answers
+      # print board
+      print @slots
+      puts @guesses.sort
+      puts "Pick a letter: "
+      input = gets.chomp.downcase
+      letter_match(input)
+      #print %x{clear} # Clears the terminal screen
     end
   end
+
+
+  def letter_match(guess)
+    answer_copy = Array.new(@answer)
+    num_match = 0
+    while answer_copy.include?(guess)
+      @guess_index = answer_copy.find_index(guess)
+      @slots[@guess_index + num_match] = answer_copy[@guess_index]
+      answer_copy.remove![@guess_index]
+      num_match += 1
+    end
+    if @slots.include?(guess) == false
+      puts "Wrong"
+      @max_wrong += 1
+      puts "#{@max_wrong} wrong guesses so far."
+      #moving ASCII art
+    end
+  end
+
+  #
+  # def letter_match(guess)
+  #   @answer.each do |l|
+  #     if guess == l
+  #       @guess_index = @answer.find_index(l)
+  #       @slots[@guess_index] = @answer[@guess_index]
+  #       puts "Good job! You found a match."
+  #       puts @answer
+  #       puts @slots
+  #     end
+  #     end
+  #     if @slots.include?(guess) == false
+  #       puts "Wrong"
+  #       @max_wrong += 1
+  #       puts "#{@max_wrong} wrong guesses so far."
+  #       #moving ASCII art
+  #     end
+  # end
 
   def game_end
 
@@ -64,16 +96,10 @@ attr_reader :answer, :guesses, :scores, :guess_index, :slots,
 end
 
 class Board
-attr_reader :slots,
+attr_reader :slots
 
   def initalize
-    @slots = []
-  end
 
-  def number_of_slots(answer_length)
-    (answer_length).times do
-    @slots.push("_")
-    end
   end
 
 end
