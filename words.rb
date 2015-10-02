44c3bc368931c829e824f32ef31e5c15aba42987
Tammy <misstamar@gmail.com>
2015-10-02 15:54:24 -0700

    Hi there

--- words.rb
+++ words.rb
@@ -1,62 +1,56 @@
 class Game
-  words = ["cat"]
+  words = ["cat", "dog", "tree", "apple"]
   _array =[]
+  @under_num = 0
+
   #fish_images = [fish1, fish2, fish3, fish4, fish5, fish6]
   answer = words[rand(words.length-1)]
   fish1 = "I am a 6-alive-fish image"
 
-puts "Hi! welcome to the Guess words game"
-puts "The fish will be fed only after you win"
-puts "Save the fish!!!"
+  puts "Hi! welcome to the Guess words game"
+  puts "The fish will be fed only after you win"
+  puts "Save the fish!!!"
   puts fish1
 
   #creat new array of underscores
-(answer.length).times do
+  (answer.length).times do
     _array.push("_")
     print "_ "
   end
 
-print _array
-
-#creat new array of letters
-answer_array = answer.split("")
 
 
+  #creat new array of letters
+  answer_array = answer.split("")
 
-count = 1
-puts
-puts "Guess a letter"
-@guess = gets.chomp
 
-while count < 6
-@check = true #  if check is true -> you won (no underscores)
-# loop that edit the _array
-
-answer_array.each_with_index do |n , i|
-    if @guess === n
-      _array[i] = @guess
-    end
-
-# check if you won
-  _array.each do |n|
-    if n === "_"
-      @check = false
-    end
-  end
-  print _array
   puts
-  print "Guess another letter"
-  puts
+  puts "Guess a letter"
   @guess = gets.chomp
+  count = 1
+  @num = 0
 
-   if @check
-     puts "Yay!!! you saved the fish!!!"
-     break
-   else
-     "Guess another letter"
-     @guess = gets.chomp
 
-  count +=1
+  while count < 7
+    answer_array.each do |n|
+      if n === @guess
+        _array[@num] = n
+        @num +=1
+        print _array
+        if (@num == answer_array.length)
+          puts
+          puts "Yay! you won and saved the fish"
+          exit
+        end
+      end
+    end
+      puts " Choose another letter"
+      @guess = gets.chomp
+    #   print _array
+
+      puts
+
+    count += 1
   end
 
 end
@@ -65,7 +59,43 @@ end
 
 
 
-end
 
-puts answer_array[0]
-end
+
+
+
+
+
+
+
+
+# # check if you won
+#   _array.each do |n|
+#     if n === "_"
+#
+#     end
+#   end
+#   print _array
+#   puts
+#   print "Guess another letter"
+#   @guess = gets.chomp
+#
+#    if @check
+#      puts "Yay!!! you saved the fish!!!"
+#      break
+#    else
+#      "Guess another letter"
+#      @guess = gets.chomp
+#
+#   count +=1
+#   end
+#
+# end
+#
+#
+#
+#
+#
+# end
+#
+# puts answer_array[0]
+# end
