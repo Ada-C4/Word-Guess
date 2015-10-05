569b781b64d1fc19b7ce281a950744d903bfea59
Desiree <desireepoland@gmail.com>
2015-10-05 10:03:09 -0700

    Add colorize

--- word-guess.rb
+++ word-guess.rb
@@ -28,16 +28,16 @@ class Game
   def print_board(guess)
     2.times {puts}
     print_image
-    print "\n\n\nWord: "
+    print "\n\n\nWord: ".light_blue
     @answer_array.each do |letter|
       if letter == guess || @correct_guesses.include?(letter)
-        print letter
+        print letter.light_blue
         @guess_array.push(letter)
       else
-        print "_"
+        print "_".light_blue
       end
     end
-    puts "\nWrong guesses:\n#{@wrong_guesses.join(" ")}"
+    puts "\nWrong guesses:\n#{@wrong_guesses.join(" ")}".light_blue
    @turn_array = @guess_array
    @guess_array = []
   end
@@ -52,23 +52,23 @@ class Game
 
   def print_image
     if @wrong_guesses.length == 0
-      puts PACMAN8
+      puts PACMAN8.light_yellow
     elsif @wrong_guesses.length == 1
-      puts PACMAN7
+      puts PACMAN7.light_yellow
     elsif @wrong_guesses.length == 2
-      puts PACMAN6
+      puts PACMAN6.light_yellow
     elsif @wrong_guesses.length == 3
-      puts PACMAN5
+      puts PACMAN5.light_yellow
     elsif @wrong_guesses.length == 4
-      puts PACMAN4
+      puts PACMAN4.light_yellow
     elsif @wrong_guesses.length == 5
-      puts PACMAN3
+      puts PACMAN3.light_yellow
     elsif @wrong_guesses.length == 6
-      puts PACMAN2
+      puts PACMAN2.light_yellow
     elsif @wrong_guesses.length == 7
-      puts PACMAN1
+      puts PACMAN1.light_yellow
     else
-      puts PACMAN
+      puts PACMAN.light_yellow
     end
   end
 
@@ -79,15 +79,15 @@ class Game
       check_guess(guess)
       print_board(guess)
       if !game_lost? && !game_won?
-        puts "\nPlease enter another letter:"
+        puts "\nPlease enter another letter:".light_cyan
       end
     end
     if game_lost?
-      puts "The secret word was: #{@answer}"
+      puts "The secret word was: #{@answer}".light_magenta
     else
       print %x{clear}
-      puts "The secret word was: #{@answer}\n\n"
-      puts PACMAN_WIN
+      puts "The secret word was: #{@answer}\n\n".light_magenta
+      puts PACMAN_WIN.light_yellow
     end
   end
 
@@ -100,10 +100,10 @@ class Game
   end
 
   def start_game
-    puts "Welcome to Word Guess!"
-    puts "Enter letters to guess the secret word before pacman eats everything!\n\n"
+    puts "Welcome to Word Guess!".bold.light_green
+    puts "Enter letters to guess the secret word before pacman eats everything!\n\n".light_green
     print_image
-    puts "\nWhat is your guess?"
+    puts "\nWhat is your guess?".light_cyan
   end
 
 end
